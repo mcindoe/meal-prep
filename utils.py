145d52ac25fa552a6a23a247c84e133686b5ac15
mcindoe <conormcindoe1@gmail.com>
2020-11-08 13:33:53 +0000

    updated utils.py

--- utils.py
+++ utils.py
@@ -13,7 +13,7 @@ def load_meals():
     return meals
 
 
-def filter_history(history, start=None, end=None)
+def filter_history(history, start=None, end=None):
     if start is not None:
         history = {
             date: entry
@@ -43,20 +43,23 @@ def load_history(start=None, end=None):
     return filter_history(history, start, end)
 
 
+def write_history(history):
+    with open(HISTORY_FILE, 'wb') as fp:
+        pickle.dump(history, fp)
+
+
 def write_history_entry(date, entry):
     history = load_history()
     history[date] = entry
-    with open(HISTORY_FILE, 'wb') as fp:
-        pickle.dump(history, fp)
+    write_history(history)
 
 
 def write_history_entries(entries):
-    'entries: list of (date, entry) tuples'
+    '''entries: dictionary of date: choice'''
     history = load_history()
-    for date, entry in entries:
+    for date, entry in entries.items():
         history[date] = entry
-    with open(HISTORY_FILE, 'wb') as fp:
-        pickle.dump(history, fp)
+    write_history(history)
 
 
 def delete_history_entries(dates):
@@ -72,6 +75,16 @@ def delete_history_entries(dates):
         for d, entry in history.items()
         if d not in dates
     }
-    with open(HISTORY_FILE, 'wb') as fp:
-        pickle.dump(history, fp)
+    write_history(history)
 
+
+def get_protein(meal_name):
+    meals = load_meals()
+    assert meal_name in meals.keys()
+    return meals[meal_name]['protein']
+
+
+def is_pasta(meal_name):
+    meals = load_meals()
+    assert meal_name in meals.keys()
+    return meals[meal_name]['pasta']
