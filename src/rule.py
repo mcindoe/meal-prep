e5b78d18331196299d50ce212cb73a125d7a2cc9
Conor <conormcindoe1@gmail.com>
2022-08-11 21:20:50 +0100

    Pass a date to Rules

--- src/rule.py
+++ src/rule.py
@@ -1,6 +1,7 @@
 from abc import ABC
 from abc import abstractmethod
 from copy import copy
+import datetime as dt
 from typing import Iterable
 
 from mealprep.src.constants import BaseEnum
@@ -12,42 +13,55 @@ from mealprep.src.meal import MealDiary
 
 class Rule(ABC):
     """
-    A Rule is a filter which, given a MealDiary, is applied to a MealCollection and
-    returns a smaller MealCollection of meals which don't violate the rule
+    A Rule is a file which can be applied to a MealCollection, with a MealDiary as context,
+    to find a smaller MealCollection which don't violate the rule, on the date, given that
+    MealDiary
     """
 
-    def __call__(self, meal_collection: MealCollection, meal_diary: MealDiary) -> MealCollection:
+    def __call__(
+        self,
+        meal_collection: MealCollection,
+        date: dt.date,
+        meal_diary: MealDiary
+    ) -> MealCollection:
+
         assert isinstance(meal_collection, MealCollection)
         assert isinstance(meal_diary, MealDiary), f"Rule was pased a {type(meal_diary)}"
 
         meal_collection = copy(meal_collection)
-        ret = self.filter(meal_collection, meal_diary)
+        ret = self.filter(meal_collection, date, meal_diary)
         assert isinstance(ret, MealCollection), f"Rule returned a {type(ret)}"
 
         return ret
 
     @staticmethod
     @abstractmethod
-    def filter(meal_collection: MealCollection, meal_diary: MealDiary) -> MealCollection:
+    def filter(meal_collection: MealCollection, date: dt.date, meal_diary: MealDiary) -> MealCollection:
         raise NotImplementedError("A Rule's filter method must be implemented")
 
 
 class RuleCollection:
     def __init__(self, rules: Iterable[Rule]):
-        assert all(issubclass(x, Rule) for x in rules)
         self.rules = tuple(x for x in rules)
+        assert all(issubclass(x, Rule) for x in self.rules)
 
-    def __call__(self, meal_collection: MealCollection, meal_diary: MealDiary) -> MealCollection:
+    def __call__(
+        self,
+        meal_collection: MealCollection,
+        date: dt.date,
+        meal_diary: MealDiary
+    ) -> MealCollection:
+
         ret = meal_collection
         for rule in self.rules:
-            ret = rule(ret)
+            ret = rule(ret, date, meal_diary)
         return ret
 
 
 # TODO: This just an example. Something we can implement when setting up the project
 class NoVegetablesRule(Rule):
     @staticmethod
-    def filter(meal_collection: MealCollection, meal_diary: MealDiary) -> MealCollection:
+    def filter(meal_collection: MealCollection, date: dt.date, meal_diary: MealDiary) -> MealCollection:
         return MealCollection(
             x for x in meal_collection.meals
             if not any(ing.category == Category.VEGETABLE for ing in x.ingredient_quantities)
