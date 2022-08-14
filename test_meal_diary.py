eead5f69c1a5175e073064bd116ea2405ed2ae6b
Conor <conormcindoe1@gmail.com>
2022-08-14 11:19:18 +0100

    Remove test files

--- test_meal_diary.py
+++
@@ -1,9 +0,0 @@
-import datetime as dt
-from mealprep.src.meal import Meal, MealDiary
-from mealprep.src.ingredient import Ingredient, IngredientQuantityCollection
-
-md = MealDiary({
-    Meal(
-        IngredientQuan
-    )
-})
