18263aa5ffcd4a9cbd2b510698f3782ccc1b30b5
Conor <conormcindoe1@gmail.com>
2022-08-14 11:21:28 +0100

    Add main launch script

---
+++ src/main.py
@@ -0,0 +1,25 @@
+import datetime as dt
+from typing import Iterable
+
+from mealprep.src.config import config
+from mealprep.src.meal import MealCollection
+from mealprep.src.meal import MealDiary
+from mealprep.src.meal_selector import MealSelector
+
+
+if __name__ == "__main__":
+    meal_collection = MealCollection.from_supported_meals()
+    rule_collection = config.rule_collection
+    meal_diary = MealDiary.from_project_diary()
+    meal_selector = MealSelector(
+        meal_collection,
+        rule_collection,
+        meal_diary
+    )
+
+    dates = config.dates
+    diary_additions = meal_selector.recommend_until_confirmed(dates)
+
+    new_diary = meal_diary.upsert(diary_additions)
+    print("Bon Appetit!")
+    new_diary.to_project_diary()
