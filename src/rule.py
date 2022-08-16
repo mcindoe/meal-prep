de2d2c76f1e68c2cfff8c125d0daf5261d067d92
Conor <conormcindoe1@gmail.com>
2022-08-17 00:02:24 +0100

    Add more rules

--- src/rule.py
+++ src/rule.py
@@ -9,6 +9,8 @@ from mealprep.src.constants import Category
 from mealprep.src.meal import Meal
 from mealprep.src.meal import MealCollection
 from mealprep.src.meal import MealDiary
+from mealprep.src.meal import MealTag
+from mealprep.src.meal import MealProperty
 
 
 class Rule(ABC):
@@ -27,6 +29,7 @@ class Rule(ABC):
 
         assert isinstance(meal_collection, MealCollection)
         assert isinstance(meal_diary, MealDiary), f"Rule was pased a {type(meal_diary)}"
+        assert date not in meal_diary.dates, f"{date.strftime('%Y-%m-%d')} is in the passed MealDiary"
 
         meal_collection = meal_collection.copy()
         ret = self.filter(meal_collection, date, meal_diary)
@@ -50,7 +53,7 @@ class Rule(ABC):
 class RuleCollection:
     def __init__(self, rules: Iterable[Rule]):
         self.rules = tuple(x for x in rules)
-        assert all(issubclass(x, Rule) for x in self.rules)
+        assert all(isinstance(x, Rule) for x in self.rules)
 
     def copy(self) -> "RuleCollection":
         return RuleCollection(copy.deepcopy(self.rules))
@@ -68,15 +71,56 @@ class RuleCollection:
         return ret
 
 
-# TODO: This just an example. Something we can implement when setting up the project
-class NoVegetablesRule(Rule):
+class NotPastaTwiceWithinFiveDaysRule(Rule):
     @staticmethod
     def filter(meal_collection: MealCollection, date: dt.date, meal_diary: MealDiary) -> MealCollection:
+        pasta_within_five_days = any(
+            meal[MealTag.PASTA]
+            for meal in meal_diary.filter_by_time_delta(date, dt.timedelta(days=5)).meals
+        )
+
+        if not pasta_within_five_days:
+            return meal_collection
+
         return MealCollection(
-            x for x in meal_collection.meals
-            if not any(ing.category == Category.VEGETABLE for ing in x.ingredient_quantities)
+            meal
+            for meal in meal_collection.meals
+            if not meal[MealTag.PASTA]
+        )
+
+
+# TODO: May require revisiting and changed to MealTemplates
+class NotSameMealWithinSevenDaysRule(Rule):
+    @staticmethod
+    def filter(meal_collection: MealCollection, date: dt.date, meal_diary: MealDiary) -> MealCollection:
+        meal_names_to_avoid = set(
+            meal.name
+            for meal in meal_diary.filter_by_time_delta(date, dt.timedelta(days=7)).meals
+        )
+
+        return MealCollection(
+            meal
+            for meal in meal_collection.meals
+            if meal.name not in meal_names_to_avoid
+        )
+
+
+class NotSameMeatOnConsecutiveDaysRule(Rule):
+    @staticmethod
+    def filter(meal_collection: MealCollection, date: dt.date, meal_diary: MealDiary) -> MealCollection:
+        meats_to_avoid = set(
+            meal[MealProperty.MEAT]
+            for meal in meal_diary.filter_by_time_delta(date, dt.timedelta(days=1)).meals
+        )
+
+        return MealCollection(
+            meal
+            for meal in meal_collection.meals
+            if meal[MealProperty.MEAT] not in meats_to_avoid
         )
 
 
 class Rules(BaseEnum):
-    NO_VEGETABLES = NoVegetablesRule()
+    NOT_PASTA_TWICE_WITHIN_FIVE_DAYS = NotPastaTwiceWithinFiveDaysRule()
+    NOT_SAME_MEAL_WITHIN_SEVEN_DAYS = NotSameMealWithinSevenDaysRule()
+    NOT_SAME_MEAT_ON_CONSECUTIVE_DAYS = NotSameMeatOnConsecutiveDaysRule()
