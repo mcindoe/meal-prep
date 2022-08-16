85421706469d180f144514a0a7266ad39c85aedb
Conor <conormcindoe1@gmail.com>
2022-08-17 00:45:55 +0100

    Add exit in main

--- src/main.py
+++ src/main.py
@@ -6,7 +6,6 @@ from mealprep.src.meal import MealCollection
 from mealprep.src.meal import MealDiary
 from mealprep.src.meal_selector import MealSelector
 
-
 if __name__ == "__main__":
     meal_collection = MealCollection.from_supported_meals()
     rule_collection = config.rule_collection
@@ -19,6 +18,9 @@ if __name__ == "__main__":
 
     dates = config.dates
     diary_additions = meal_selector.recommend_until_confirmed(dates)
+
+    if diary_additions is None:
+        exit()
 
     new_diary = meal_diary.upsert(diary_additions)
     print("Bon Appetit!")
