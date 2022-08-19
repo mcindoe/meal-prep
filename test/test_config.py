06a38821e5298f59687d9d524f2cc0fbc7ac8dfa
mcindoe <conormcindoe1@gmail.com>
2022-08-19 13:11:57 +0100

    Removed redundant comments

--- test/test_config.py
+++ test/test_config.py
@@ -1,16 +1,7 @@
-"""
-Unit tests for the Config class
-"""
-
 from mealprep.src.config import config
 from mealprep.src.constants import ConfigEntries
 
-
 def test_config():
-    """
-    Unit tests for the Config class
-    """
-
     for entry_name in ConfigEntries.values():
         entry_value = getattr(config, entry_name)
 
