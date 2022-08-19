e449448a27798ac2e65f0e2efcd4314d1764e8ba
mcindoe <conormcindoe1@gmail.com>
2022-08-19 14:03:57 +0100

    Add unit tests for the Meal class

--- test/test_meal.py
+++ test/test_meal.py
@@ -1,6 +1,115 @@
+import pytest
+
+from mealprep.src.constants import MealMeat
+from mealprep.src.constants import MealProperty
+from mealprep.src.constants import MealTag
+from mealprep.src.constants import Unit
+from mealprep.src.ingredient import IngredientQuantity
+from mealprep.src.ingredient import IngredientQuantityCollection
+from mealprep.src.ingredient import Ingredients
 from mealprep.src.meal import Meal
 from mealprep.src.meal import MealCollection
 from mealprep.src.meal import MealDiary
+
+
+class TestMeal:
+    def test_initialiser_errors(self):
+        # Name is not a string
+        with pytest.raises(TypeError):
+            Meal(
+                name=42,
+                ingredient_quantities=(
+                    IngredientQuantity(Ingredients.BAY_LEAVES, Unit.BOOL, True),
+                    IngredientQuantity(Ingredients.CARROT, Unit.NUMBER, 1),
+                ),
+                properties={},
+                tags=(MealTag.PASTA,)
+            )
+
+        # Ingredient Quantities contains non-Ingredients
+        with pytest.raises(TypeError):
+            Meal(
+                name="Meal Name",
+                ingredient_quantities=(
+                    "300g of Carrot",
+                ),
+                properties={
+                    MealProperty.MEAT: MealMeat.BEEF
+                },
+                tags=(MealTag.PASTA,)
+            )
+
+        # Properties not a dictionary
+        with pytest.raises(TypeError):
+            Meal(
+                name="Meal Name",
+                ingredient_quantities=(
+                    IngredientQuantity(Ingredients.BAY_LEAVES, Unit.BOOL, True),
+                    IngredientQuantity(Ingredients.CARROT, Unit.NUMBER, 1),
+                ),
+                properties=(MealMeat.BEEF,),
+                tags=(MealTag.PASTA,)
+            )
+
+        # Missing properties
+        with pytest.raises(ValueError):
+            Meal(
+                name="Meal Name",
+                ingredient_quantities=(
+                    IngredientQuantity(Ingredients.BAY_LEAVES, Unit.BOOL, True),
+                    IngredientQuantity(Ingredients.CARROT, Unit.NUMBER, 1),
+                ),
+                properties={},
+                tags=(MealTag.PASTA,)
+            )
+
+        # Tags argument contains non-Tags
+        with pytest.raises(ValueError):
+            Meal(
+                name="Meal Name",
+                ingredient_quantities=(
+                    IngredientQuantity(Ingredients.BAY_LEAVES, Unit.BOOL, True),
+                    IngredientQuantity(Ingredients.CARROT, Unit.NUMBER, 1),
+                ),
+                properties={
+                    MealProperty.MEAT: MealMeat.BEEF
+                },
+                tags=("PASTA",)
+            )
+
+    def test_getters(self):
+        x = Meal(
+            name="Meal Name",
+            ingredient_quantities=(
+                IngredientQuantity(Ingredients.BAY_LEAVES, Unit.BOOL, True),
+                IngredientQuantity(Ingredients.CARROT, Unit.NUMBER, 1),
+            ),
+            properties={
+                MealProperty.MEAT: MealMeat.BEEF
+            },
+            tags=(MealTag.PASTA,)
+        )
+
+        assert x.name == "Meal Name"
+        assert isinstance(x.ingredient_quantities, IngredientQuantityCollection)
+        assert x[MealProperty.MEAT] is MealMeat.BEEF
+        assert x[MealTag.PASTA]
+        assert not x[MealTag.VEGETARIAN]
+
+        chinese_pork_belly = Meal.from_name("Sticky Chinese Pork Belly")
+        assert chinese_pork_belly[MealProperty.MEAT] is MealMeat.PORK
+        assert not chinese_pork_belly[MealTag.VEGETARIAN]
+
+        # Construction from a single tag
+        Meal(
+            name="Meal Name",
+            ingredient_quantities=(),
+            properties={
+                MealProperty.MEAT: MealMeat.BEEF
+            },
+            tags=MealTag.PASTA
+        )
+
 
 
 class TestMealCollection:
