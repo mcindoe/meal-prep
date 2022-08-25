5689e7aeb8e10a59a605b27b89e3f65f71cc9f47
mcindoe <conormcindoe1@gmail.com>
2022-08-25 14:06:22 +0100

    Rename collection -> items in BasicIterator

--- src/basic_iterator.py
+++ src/basic_iterator.py
@@ -3,14 +3,14 @@ class BasicIterator:
 	Generic Iterator class for iterating over a collection of items
 	"""
 
-	def __init__(self, collection):
-		self.collection = collection
+	def __init__(self, items):
+		self.items = items
 		self.index = 0
 
 	def __next__(self):
-		if self.index >= len(self.collection):
+		if self.index >= len(self.items):
 			raise StopIteration
 
-		ret = self.collection[self.index]
+		ret = self.items[self.index]
 		self.index += 1
 		return ret
