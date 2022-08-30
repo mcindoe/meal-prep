31d0eeb85b8dd2fab4ae0ff93bb3972c42303150
Conor <conormcindoe1@gmail.com>
2022-08-30 23:35:45 +0100

    Add meal unit tests

--- test/test_meal.py
+++ test/test_meal.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import pytest
 
 from mealprep.src.constants import MealMeat
@@ -26,19 +27,6 @@ class TestMeal:
                 tags=(MealTag.PASTA,)
             )
 
-        # Ingredient Quantities contains non-Ingredients
-        with pytest.raises(TypeError):
-            Meal(
-                name="Meal Name",
-                ingredient_quantities=(
-                    "300g of Carrot",
-                ),
-                properties={
-                    MealProperty.MEAT: MealMeat.BEEF
-                },
-                tags=(MealTag.PASTA,)
-            )
-
         # Properties not a dictionary
         with pytest.raises(TypeError):
             Meal(
@@ -51,6 +39,18 @@ class TestMeal:
                 tags=(MealTag.PASTA,)
             )
 
+        # Properties contains non-properties
+        with pytest.raises(TypeError):
+            Meal(
+                name="Meal Name",
+                ingredient_quantities=(
+                    IngredientQuantity(Ingredients.BAY_LEAVES, Unit.BOOL, True),
+                    IngredientQuantity(Ingredients.CARROT, Unit.NUMBER, 1),
+                ),
+                properties=({"Meat": "Beef"}),
+                tags=(MealTag.PASTA,)
+            )
+
         # Missing properties
         with pytest.raises(ValueError):
             Meal(
@@ -60,6 +60,19 @@ class TestMeal:
                     IngredientQuantity(Ingredients.CARROT, Unit.NUMBER, 1),
                 ),
                 properties={},
+                tags=(MealTag.PASTA,)
+            )
+
+        # Ingredient Quantities contains non-Ingredients
+        with pytest.raises(TypeError):
+            Meal(
+                name="Meal Name",
+                ingredient_quantities=(
+                    "300g of Carrot",
+                ),
+                properties={
+                    MealProperty.MEAT: MealMeat.BEEF
+                },
                 tags=(MealTag.PASTA,)
             )
 
@@ -100,8 +113,10 @@ class TestMeal:
         assert chinese_pork_belly[MealProperty.MEAT] is MealMeat.PORK
         assert not chinese_pork_belly[MealTag.VEGETARIAN]
 
+
+    def test_init_from_single_tag(self):
         # Construction from a single tag
-        Meal(
+        x = Meal(
             name="Meal Name",
             ingredient_quantities=(
                 IngredientQuantity(Ingredients.BAY_LEAVES, Unit.BOOL, True),
@@ -113,14 +128,151 @@ class TestMeal:
             tags=MealTag.PASTA
         )
 
+        assert isinstance(x, Meal)
+
+    def test_construction_from_name(self):
+        meal = Meal.from_name("Spaghetti Bolognese")
+
+        assert isinstance(meal, Meal)
+        assert meal[MealProperty.MEAT] is MealMeat.BEEF
+
 
 class TestMealCollection:
-    def test_initialiser(self):
-        meals = (
+    @pytest.fixture()
+    def meals(self):
+        yield (
             Meal.from_name("Spaghetti Bolognese"),
-            Meal.from_name("Sticky Chinese Pork Belly")
+            Meal.from_name("Sticky Chinese Pork Belly"),
         )
-        meal_collection = MealCollection(meals)
 
+    @pytest.fixture()
+    def meal_collection(self, meals):
+        yield MealCollection(meals)
+
+    def test_initialiser(self, meals, meal_collection):
         # MealCollections should maintain their own copy of their meals
         assert meal_collection.meals is not meals
+
+        # One of the collection is not a Meal
+        with pytest.raises(TypeError):
+            MealCollection(meals + ("Lasagne",))
+
+    def test_properties(self, meal_collection):
+        assert len(meal_collection) == 2
+        assert bool(meal_collection)
+
+        empty_collection = MealCollection()
+        assert not bool(empty_collection)
+        assert not empty_collection
+
+    def test_indexing(self, meals, meal_collection):
+        assert meal_collection[0] is meals[0]
+        assert meal_collection[1] is meals[1]
+
+
+class TestMealDiary:
+    @pytest.fixture()
+    def meal_diary_dict(self):
+        yield {
+            dt.date(2022, 1, 1): Meal.from_name("Spaghetti Bolognese"),
+            dt.date(2022, 1, 10): Meal.from_name("Lasagne"),
+            dt.date(2022, 1, 11): Meal.from_name("Kedgeree")
+        }
+
+    @pytest.fixture()
+    def meal_diary(self, meal_diary_dict):
+        yield MealDiary(meal_diary_dict)
+
+    def test_initialiser(self, meal_diary_dict, meal_diary):
+        assert isinstance(meal_diary, MealDiary)
+
+        # MealDiaries should create a copy of the passed dictionary
+        assert meal_diary.meal_diary is not meal_diary_dict
+
+    def test_initialiser_errors(self):
+        # Keys are not all datetime.dates
+        with pytest.raises(TypeError):
+            MealDiary({
+                "2022-01-01": Meal.from_name("Spaghetti Bolognese"),
+            })
+
+        # Values are not all Meals
+        with pytest.raises(TypeError):
+            MealDiary({
+                dt.date(2022, 1, 1): "Spaghetti Bolognese",
+            })
+
+    def test_copy(self, meal_diary):
+        assert meal_diary.meal_diary == meal_diary.copy().meal_diary
+        assert meal_diary.meal_diary is not meal_diary.copy().meal_diary
+
+    def test_indexing(self, meal_diary):
+        with pytest.raises(TypeError):
+            meal_diary["2022-01-01"]
+
+        assert meal_diary[dt.date(2022, 1, 1)] == Meal.from_name("Spaghetti Bolognese")
+
+        altered_diary = meal_diary.copy()
+
+        # Setting with a non-date key is not allowed
+        with pytest.raises(TypeError):
+            altered_diary["2022-01-01"] = Meal.from_name("Spaghetti Bolognese")
+
+        # Setting with a non-meal value is not allowed
+        with pytest.raises(TypeError):
+            altered_diary[dt.date(2022, 1, 1)] = "Spaghetti Bolognese"
+
+        altered_diary[dt.date(2022, 2, 1)] = Meal.from_name("Sticky Chinese Pork Belly")
+        assert len(altered_diary.meal_diary) == 4
+        assert dt.date(2022, 2, 1) in altered_diary.dates
+
+    def test_loading_project_diary(self):
+        project_diary = MealDiary.from_project_diary()
+        assert isinstance(project_diary, MealDiary)
+
+    def test_upsert(self, meal_diary):
+        addition = MealDiary({
+            dt.date(2022, 2, 1): Meal.from_name("Sticky Chinese Pork Belly")
+        })
+        altered_diary = meal_diary.copy()
+
+        assert altered_diary.upsert(addition).meal_diary is not altered_diary.meal_diary
+        assert dt.date(2022, 2, 1) in altered_diary.upsert(addition).dates
+
+    def test_difference(self, meal_diary):
+        overlapping_diary = MealDiary({
+            dt.date(2022, 1, 1): Meal.from_name("Spaghetti Bolognese"),
+        })
+        difference_diary = meal_diary.difference(overlapping_diary)
+
+        assert len(difference_diary.meal_diary) == 2
+        assert dt.date(2022, 1, 1) not in difference_diary.dates
+        assert dt.date(2022, 1, 10) in difference_diary.dates
+        assert Meal.from_name("Spaghetti Bolognese") not in difference_diary.meals
+        assert Meal.from_name("Kedgeree") in difference_diary.meals
+
+    def test_filter_by_time_delta(self, meal_diary):
+        expected_diary = MealDiary({
+            dt.date(2022, 1, 10): Meal.from_name("Lasagne"),
+            dt.date(2022, 1, 11): Meal.from_name("Kedgeree")
+        })
+        observed_diary = meal_diary.filter_by_time_delta(dt.date(2022, 1, 10), dt.timedelta(days=1))
+
+        assert expected_diary.meal_diary == observed_diary.meal_diary
+
+    def test_except_dates(self, meal_diary):
+        expected_diary = MealDiary({
+            dt.date(2022, 1, 1): Meal.from_name("Spaghetti Bolognese"),
+            dt.date(2022, 1, 11): Meal.from_name("Kedgeree")
+        })
+
+        # Should be able to exclude either a single date or an iterable of dates
+        observed_diary_1 = meal_diary.except_dates(dt.date(2022, 1, 10),)
+        observed_diary_2 = meal_diary.except_dates(dt.date(2022, 1, 10))
+
+        assert observed_diary_1.meal_diary == observed_diary_2.meal_diary
+        assert expected_diary.meal_diary == observed_diary_1.meal_diary
+
+        with pytest.raises(TypeError):
+            # Passed dates must be datetime.dates
+            meal_diary.except_dates("2022-01-01")
