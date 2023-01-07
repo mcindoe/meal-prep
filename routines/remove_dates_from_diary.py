0d5d5dd59c3e25d494460c45e27ebda8d52a14d5
Conor McIndoe <34808649+mcindoe@users.noreply.github.com>
2023-01-07 14:28:12 +0000

    10 feature routine to generate shopping list independently of recommend routine (#16)
    * Move filter_by_max_before_and_max_after_today to MealDiary
    
    * Add routine to make shopping list

--- routines/remove_dates_from_diary.py
+++ routines/remove_dates_from_diary.py
@@ -21,27 +21,10 @@ if __name__ == "__main__":
         print("Project meal diary is empty, so nothing to do")
         exit()
 
-    previous_dates = sorted([x for x in meal_diary.dates if x < dt.date.today()])
-    if previous_dates:
-        n_previous_dates_printed = min(len(previous_dates), MAX_PRINTED_PREVIOUS_DIARY_ENTRIES)
-        if n_previous_dates_printed > 0:
-            min_printed_date = previous_dates[-n_previous_dates_printed]
-        else:
-            min_printed_date = dt.date.today()
-    else:
-        min_printed_date = dt.date.today()
-
-    next_dates = sorted([x for x in meal_diary.dates if x >= dt.date.today()])
-    if next_dates:
-        n_next_dates_printed = min(len(next_dates), MAX_PRINTED_NEXT_DIARY_ENTRIES)
-        if n_next_dates_printed > 0:
-            max_printed_date = next_dates[n_next_dates_printed - 1]
-        else:
-            max_printed_date = dt.date.today()
-    else:
-        max_printed_date = None
-
-    printed_diary = meal_diary.filter_dates(min_date=min_printed_date, max_date=max_printed_date)
+    printed_diary = meal_diary.filter_by_max_before_and_max_after_today(
+        MAX_PRINTED_PREVIOUS_DIARY_ENTRIES,
+        MAX_PRINTED_NEXT_DIARY_ENTRIES
+    )
 
     if printed_diary:
         print("Recent diary:\n")
