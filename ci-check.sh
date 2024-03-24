325d8380b773684dd9dd388720b75e93a4832c2e
Conor McIndoe <conormcindoe1@gmail.com>
2024-03-24 12:48:19 +0000

    Add ci-check.sh

---
+++ ci-check.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/bash
+
+black . --line-length 100 --check
+isort . --check --line-length 100 --multi-line 3 --lines-after-imports 2 --force-sort-within-sections --dont-order-by-type --trailing-comma
