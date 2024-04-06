f72b0f1c3a043755af3e9bf5f51227091768f2d4
Conor McIndoe <conormcindoe1@gmail.com>
2024-04-06 09:34:23 +0100

    Make MEALS_DIR entry in loc.py

--- mealprep/loc.py
+++ mealprep/loc.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 ROOT_DIR = Path(__file__).parents[1]
 DATA_DIR = ROOT_DIR / "data"
+MEALS_DIR = DATA_DIR / "meals"
 SHOPPING_LIST_DIR = DATA_DIR / "shoppingLists"
 
 SUPPORTED_INGREDIENT_INFO_FILE = DATA_DIR / "ingredients.csv"
--- mealprep/write_meals.py
+++ mealprep/write_meals.py
@@ -1,12 +1,9 @@
-from mealprep.loc import ROOT_DIR
+from mealprep.loc import MEALS_DIR
 from mealprep.meal import Meals
 from mealprep.recipe.writing import write_meal_as_recipe
 
 
-MEAL_DIR = ROOT_DIR / "data/meals"
-
-
 for meal in Meals.values():
     file_name = f"{meal.name}.yaml"
-    file_path = MEAL_DIR / file_name
+    file_path = MEALS_DIR / file_name
     write_meal_as_recipe(meal, file_path)
