e2dab403f0e032858032e6148bff8b9919b44c77
Conor McIndoe <conormcindoe1@gmail.com>
2024-04-14 12:22:32 +0100

    Rename routines to app

--- mealprep/app/ci/ci-check.sh
+++ mealprep/app/ci/ci-check.sh
@@ -2,5 +2,5 @@
 
 black . --line-length 100 --check
 isort . --check --line-length 100 --multi-line 3 --lines-after-imports 2 --force-sort-within-sections --dont-order-by-type --trailing-comma
-python -m mealprep.routines.ci.check_ingredient_definitions
-python -m mealprep.routines.ci.check_recipe_definitions
+python -m mealprep.app.ci.check_ingredient_definitions
+python -m mealprep.app.ci.check_recipe_definitions
